2f34da64adfdaf06f531496a5eaa06cc355bad59
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-23 12:08:57 +0530

    ADDED : Store Results In Dictionary

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -3,7 +3,10 @@
 echo "Arithmetic Operation And Sorting"
 read -p "Enter three number : " a b c
 echo "$a $b $c"
-echo "scale=2;$a+($b*$c)" | bc 
-echo "scale=2;($a*$b)+$c" | bc
-echo "scale=3;$c+($a/$b)" | bc
-echo "scale=2;($a%$b)+$c" | bc
+
+declare -A results
+results["operationOne"]=`echo "scale=2;$a+($b*$c)" | bc ` 
+results["operationTwo"]=`echo "scale=2;($a*$b)+$c" | bc `
+results["operationThree"]=`echo "scale=3;$c+($a/$b)" | bc `
+results["operationFour"]=`echo "scale=2;($a%$b)+$c" | bc `
+echo ${results[@]}
