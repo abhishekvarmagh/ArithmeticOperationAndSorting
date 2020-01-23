aa22f29ac6112721af9d8aecf530ccab348c5301
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-23 12:58:56 +0530

    ADDED : Store Dictionary Value into Array

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,12 +1,22 @@
 #! /bin/bash -x
 
+count=0
+declare -A results
+
 echo "Arithmetic Operation And Sorting"
 read -p "Enter three number : " a b c
 echo "$a $b $c"
 
-declare -A results
-results["operationOne"]=`echo "scale=2;$a+($b*$c)" | bc ` 
-results["operationTwo"]=`echo "scale=2;($a*$b)+$c" | bc `
-results["operationThree"]=`echo "scale=3;$c+($a/$b)" | bc `
-results["operationFour"]=`echo "scale=2;($a%$b)+$c" | bc `
+results[operation1]=`echo "scale=2;$a+($b*$c)" | bc ` 
+results[operation2]=`echo "scale=2;($a*$b)+$c" | bc `
+results[operation3]=`echo "scale=3;$c+($a/$b)" | bc `
+results[operation4]=`echo "scale=2;($a%$b)+$c" | bc `
 echo ${results[@]}
+
+while [[ $count -ne ${#results[@]} ]]
+do
+	arr[$count]=${results[operation"$(( count+1 ))"]}
+	count=$(( count+1 ))
+done
+echo ${arr[@]}
+
